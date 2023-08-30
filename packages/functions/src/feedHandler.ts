96d18d3303d5738f09b9056f52e9a0208db23c4b
Jasdeep Madan <jasdeep.singh@brighte.com.au>
2023-08-30 17:32:33 +1000

    fixed the category assignment when no category is present

--- packages/functions/src/feedHandler.ts
+++ packages/functions/src/feedHandler.ts
@@ -1,32 +1,34 @@
 import { SQSEvent } from "aws-lambda";
-import { AttributeValue, DynamoDB, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { dbClient } from "./utils/dbClient";
 import { Table } from "sst/node/table";
 import fetchRSSFeed from "./utils/fetchRssFeed";
 import { formatItem } from "./utils/formatItem";
 
-const dynamoDb = new DynamoDB({
-  region: "ap-southeast-2",
-});
-
 export async function main(event: SQSEvent) {
-  const itemTableName = Table.item.tableName;
-  const records: any[] = event.Records;
-  const { publisher, feedUrl } = JSON.parse(records[0].body);
+  try {
+    const itemTableName = Table.item.tableName;
+    const records: any[] = event.Records;
+    const { publisher, feedUrl } = JSON.parse(records[0].body);
 
-  const rssItems = await fetchRSSFeed(publisher, feedUrl);
-  for (const item of rssItems) {
-    const feedItem = await formatItem(item, publisher);
-    const putParams = new PutItemCommand({
-      TableName: itemTableName,
-      Item: { ...feedItem },
-    });
-    await dynamoDb.send(putParams);
+    const rssItems = await fetchRSSFeed(publisher, feedUrl);
+    console.log("rssItems: ", publisher, rssItems);
+    for (const item of rssItems) {
+      const feedItem = await formatItem(item, publisher);
+      const putParams = new PutItemCommand({
+        TableName: itemTableName,
+        Item: { ...feedItem },
+      });
+      await dbClient.send(putParams);
+    }
+
+    console.log(`Message processed: "${records[0].body}"`);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ status: "successful" }),
+    };
+  } catch (err) {
+    console.log("err........", err);
   }
-
-  console.log(`Message processed: "${records[0].body}"`);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ status: "successful" }),
-  };
 }
--- packages/functions/src/utils/formatItem.ts
+++ packages/functions/src/utils/formatItem.ts
@@ -1,7 +1,12 @@
 import { AttributeValue } from "@aws-sdk/client-dynamodb";
 
 export const formatItem = (item: any, publisher: string) => {
-  const categories = item.category && item.category;
+  let categories = "";
+  if (item.category && item.category.length > 1 && typeof item.category !== "string") {
+    categories = item.category.join(",");
+  } else if (item.category && typeof item.category === "string") {
+    categories = item.category;
+  }
   const feedItem: Record<any, AttributeValue> = {
     publishedDate: { S: new Date(item.pubDate).toISOString().split("T")[0] },
     title: { S: encodeURI(item.title) },
@@ -9,13 +14,14 @@ export const formatItem = (item: any, publisher: string) => {
     pubDate: { N: new Date(item.pubDate).getTime().toString() },
     author: { S: item["dc:creator"] },
     guid: { S: encodeURI(item.guid) },
-    category: { S: categories ?? "" },
+    category: { S: categories },
     publisher: { S: publisher },
   };
 
   switch (publisher) {
     case "Hacker News":
       feedItem.description = { S: "" };
+      break;
     default:
       feedItem.description = { S: encodeURI(item.description) };
   }
