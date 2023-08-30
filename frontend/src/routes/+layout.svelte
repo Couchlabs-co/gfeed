c224f201143fb881ce380f7ecbfbad3e7298f44f
Jasdeep Madan <jasdeep.singh@brighte.com.au>
2023-08-30 17:18:51 +1000

    removed skeleton UI and using daily UI

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,4 @@
 <script>
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-	// This contains the bulk of Skeletons required styles:
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
-
 	import '../app.css';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,21 @@
-/* eslint-disable */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './src/**/*.{html,js,svelte,ts}',
-    require('path').join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    ...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
-  ],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true,
+  },
 }
 
