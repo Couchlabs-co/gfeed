45b9410cb3925f98c1d98e8a64b80f71e9ec04c1
Jasdeep Madan <jmadan@protonmail.com>
2023-09-02 19:18:08 +1000

    updated header and footer components

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -1,3 +1,4 @@
+<!-- <div class="container mx-auto"> -->
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
 	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 		<a href="/" class="flex items-center">
@@ -73,3 +74,4 @@
 		</div>
 	</div>
 </nav>
+<!-- </div> -->
