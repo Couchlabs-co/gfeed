2b3ef44b5ae421d18014100bee611425e638f66b
Jasdeep Madan <jmadan@protonmail.com>
2023-09-09 21:59:05 +1000

    adding user api; saving user interests

--- packages/functions/src/user.ts
+++ packages/functions/src/user.ts
@@ -1,13 +1,53 @@
-// import { QueryCommand } from "@aws-sdk/client-dynamodb";
-// import { dbClient } from "./utils/dbClient";
+import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { dbClient } from "./utils/dbClient";
 import { ApiHandler } from "sst/node/api";
-// import { Table } from "sst/node/table";
+import { Table } from "sst/node/table";
+import * as uuid from "uuid";
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
 
-export const handler = ApiHandler(async (evt) => {
+interface UserAction {
+  title: string;
+  type: string;
+  action: string;
+}
+
+export const handler = ApiHandler(async (evt: APIGatewayProxyEventV2) => {
   console.log("evt time: ", evt.requestContext.time);
+  const body: UserAction = JSON.parse(evt.body ?? '');
+
+
+  if(!body || !body.title) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({"msg": "Bad Request"})
+    };
+  }
+
+  try {
+    const userInterestsTable = Table.UsersInterest.tableName;
+    const command: PutItemCommand = new PutItemCommand({
+      TableName: userInterestsTable,
+      Item: {
+        id: { S: uuid.v4() },
+        userId: { S: "1" },
+        interest: { S: body?.title },
+        type: { S: body?.type },
+        action: { S: body?.action },
+      },
+    });
+    const res = await dbClient.send(command);
+    return {
+      statusCode: 201,
+      body: JSON.stringify({"msg": "Success"})
+    };
+    
+  } catch (err) {
+    console.log("err........", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({"err": "Something went wrong"})
+    };
+  }
+
   
-  return {
-    statusCode: 200,
-    body: JSON.stringify({}),
-  };
 });
