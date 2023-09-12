e7bfde3a07b7849cdd2f82d7946ddfd213e9f380
Jasdeep Madan <jmadan@protonmail.com>
2023-09-12 22:44:12 +1000

    updated header and added profile pic when logged in

---
+++ frontend/src/lib/components/ProfileHeader.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let pic = '';
+</script>
+
+<label for="profile_pic" class="btn btn-ghost btn-circle avatar">
+    <div class="w-10 rounded-full">
+      <img id="profile_pic" alt="profilepic" src={pic} />
+    </div>
+</label>
