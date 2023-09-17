3b1b7aaacf71e718b882e7040e91adfeff8724b6
Jasdeep Madan <jmadan@protonmail.com>
2023-09-17 22:46:06 +1000

    removed social buttons

--- frontend/src/lib/components/feed/ListItem.svelte
+++ frontend/src/lib/components/feed/ListItem.svelte
@@ -74,7 +74,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="0.75em" height="1em" viewBox="0 0 384 512" {...$$props}><path fill="currentColor" d="M0 512V48C0 21.49 21.49 0 48 0h288c26.51 0 48 21.49 48 48v464L192 400L0 512z"/></svg>
                 </button>
                 
-                <SocialButtons />
+                <!-- <SocialButtons /> -->
         </div>
     </div>
     
