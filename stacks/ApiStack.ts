3cdacf8f9da070e38143be4ec2a5d5ad0c09b036
Jasdeep Madan <jmadan@protonmail.com>
2023-09-20 07:55:40 +1000

    added user interests route

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -1,10 +1,11 @@
 import { StackContext, use, Api } from "sst/constructs";
 import { DbStack } from "./DbStack";
 
-export function ApiStack({ stack }: StackContext) {
+export function ApiStack({ stack, app }: StackContext) {
   const { ArticleTable, UserTable, UsersInterestTable, BookmarkTable } = use(DbStack);
 
   const ReadingCornerAPI = new Api(stack, "ReadingCornerAPI", {
+    // customDomain: app.stage === "prod" ? "api.jasdeep.me" : `api-${app.stage}.jasdeep.me`,
     defaults: {
       function: {
         bind: [ArticleTable, UserTable, UsersInterestTable, BookmarkTable],
@@ -15,6 +16,7 @@ export function ApiStack({ stack }: StackContext) {
     routes: {
       "GET /feed": "packages/functions/src/feed.handler",
       "POST /users": "packages/functions/src/users.handler",
+      "GET /users/{userId}/interests": "packages/functions/src/userInterests.handler",
       "POST /user/action": "packages/functions/src/user.handler",
       "POST /user/action/bookmark": "packages/functions/src/user.handler",
       "POST /user/action/dislike": "packages/functions/src/user.handler",
