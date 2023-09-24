419778137c6ff622dae6223ed5c96cb1a1ad0dc7
Jasdeep Madan <jmadan@protonmail.com>
2023-09-24 20:19:05 +0530

    returning grouped response of interest by Type and Action

--- packages/functions/src/userInterests.ts
+++ packages/functions/src/userInterests.ts
@@ -1,14 +1,18 @@
-import { QueryCommand } from "@aws-sdk/client-dynamodb";
+import { QueryCommand, QueryCommandOutput } from "@aws-sdk/client-dynamodb";
 import { dbClient } from "./utils/dbClient";
 import { ApiHandler } from "sst/node/api";
 import { Table } from "sst/node/table";
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 
+interface Interest {
+  interest: string;
+  action: 'likes' | 'dislikes';
+  type: string;
+}
 
 export const handler = ApiHandler(async (evt: APIGatewayProxyEventV2) => {
   console.log("evt time: ", evt.requestContext.time);
   const user_id = evt.pathParameters?.userId;
-
 
   if(!user_id) {
     return {
@@ -26,10 +30,38 @@ export const handler = ApiHandler(async (evt: APIGatewayProxyEventV2) => {
         ":userId": { S: user_id },
       },
     });
-    const res = await dbClient.send(command);
+    const res: QueryCommandOutput = await dbClient.send(command);
+    const interests: Array<Interest> =[];
+    if(res.Items){
+      for(const item of res.Items){
+        interests.push({
+          interest: item.interest.S,
+          action: item.action.S,
+          type: item.type.S
+        })
+      }
+    }
+    const interestsByType = interests.reduce((acc, item) => {
+      const interestType: string = item.interest;
+      if(interestType !== undefined && !acc[interestType]) {
+        acc[interestType] = [];
+      }
+      acc[interestType].push(item)
+      return acc; 
+    }, {} as Interest);
+
+    const interestsByAction = interests.reduce((acc, item) => {
+      const interestAction = item.action;
+      if(!acc[interestAction]) {
+        acc[interestAction] = [];
+      }
+      acc[interestAction].push(item)
+      return acc; 
+    }, {});
+
     return {
       statusCode: 201,
-      body: JSON.stringify({"message": "Success", "data": res.Items})
+      body: JSON.stringify({"message": "Success", "data": {interestsByType, interestsByAction}})
     };
     
   } catch (err) {
