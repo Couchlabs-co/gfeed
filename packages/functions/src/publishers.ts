00771b129e204e60f05895e7308e7b8b770b28c8
Jasdeep Madan <jmadan@protonmail.com>
2023-09-25 22:54:44 +0530

    fixed get publishers endpoint

--- packages/functions/src/publishers.ts
+++ packages/functions/src/publishers.ts
@@ -1,4 +1,4 @@
-import { QueryCommand } from "@aws-sdk/client-dynamodb";
+import { ScanCommand, ScanCommandOutput } from "@aws-sdk/client-dynamodb";
 import { dbClient } from "./utils/dbClient";
 import { ApiHandler } from "sst/node/api";
 import { Table } from "sst/node/table";
@@ -6,15 +6,15 @@ import { Table } from "sst/node/table";
 export const handler = ApiHandler(async (evt) => {
   console.log("evt time: ", evt.requestContext.time);
   
-  const command: QueryCommand = new QueryCommand({
+  const scanCommand: ScanCommand = new ScanCommand({
     TableName: Table.feed.tableName,
-    KeyConditionExpression: "feedStatus = :feedStatus",
+    FilterExpression: "feedStatus = :feedStatus",
     ExpressionAttributeValues: {
         ":feedStatus": { S: 'active' },
     },
-    ConsistentRead: true,
-    });
-  let { Count, Items } = await dbClient.send(command);
+  });
+
+  const {Count, Items}: ScanCommandOutput = await dbClient.send(scanCommand);
 
   const publishers = <any>[];
 
