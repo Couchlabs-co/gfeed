f44b761773bbcb09b16e7971876edc2ce9608808
Jasdeep Madan <jmadan@protonmail.com>
2023-10-02 16:03:05 +0530

    Displaying valid sources in source route

---
+++ frontend/src/routes/source/+layout.svelte
@@ -0,0 +1,3 @@
+<div class="flex flex-col max-w-full max-h-screen p-2">
+    <slot />
+</div>
--- frontend/src/routes/source/+page.svelte
+++ frontend/src/routes/source/+page.svelte
@@ -1,8 +1,29 @@
+<script lang="ts">
+    /** @type {import('./$types').PageData} */
+	export let data;
+
+    const { Items } = data.data;
+
+</script>
 <div>
-    <h3>
-        List of Publishers
+    <h3 class="mt-2 mb-2">
+        Some of the sources we crawl to make sure you have latest content
     </h3>
-    <ul>
-        <li>TechCrunch</li>
-    </ul>
+    <div class="h-max w-9/12 items-center place-content-center m-auto">
+        <table class="table table-pin-rows">
+        <thead>
+          <tr>
+            <th>Publishers</th>
+          </tr>
+        </thead>
+        <tbody>
+            {#each Items as item}
+                <tr>
+                    <td>{item.publisher}</td>
+                    <td><img src={item.logo} alt={item.publisher} /></td>
+                </tr>
+            {/each}
+        </tbody>
+        </table>
+      </div>
 </div>
