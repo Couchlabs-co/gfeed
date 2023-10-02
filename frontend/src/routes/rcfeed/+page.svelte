d9812dcb27cc36779212358ee4a697f3db58a57c
Jasdeep Madan <jmadan@protonmail.com>
2023-10-02 16:06:40 +0530

    implemented publisher filter for feed

--- frontend/src/routes/rcfeed/+page.svelte
+++ frontend/src/routes/rcfeed/+page.svelte
@@ -1,21 +1,84 @@
 <script lang="ts">
+	import { readingListStore } from '../../store';
 	import ListItem from '$lib/components/feed/ListItem.svelte';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
-	const { res, session } = data;
-	const { Count, Items } = res;
+	const { publishers, feed, session } = data;
+
+	let readingFeed: any = feed.Items;
 
 	const user_id = session.user?.id.split('|')[1];
 
+	if(feed.count){
+		console.log('feed: ', feed.Count);
+		$readingListStore.articles = feed.Items;
+		readingFeed = feed.Items;
+	}
+
+	const filterFeed = (e: any) => {
+		const publisher = e.target.value;
+		console.log('publisher: ', publisher);
+		if(publisher !== 'Publishers'){
+			readingFeed = feed.Items.filter((item: any) => item.publisher === publisher);
+		}
+	}
+
 </script>
 
-<div class="w-full rounded p-2">
-	{#if Count === 0}
-		<p class="text-center">No articles found.</p>
+<div class="flex flex-row">
+	{#if feed.Count === 0}
+		<div class="w-10/12 m-auto">
+			<h2 class="notFoundText">No articles found.</h2>
+		</div>
+	{/if}
+	{#if !feed.Count}
+		<span class="loading loading-infinity loading-lg"></span>
 	{:else}
-		{#each Items as Item}
-			<ListItem Item ={Item} userId={user_id}/>
-		{/each}
+		<div class="flex flex-col w-10/12">
+			{#each readingFeed as Item}
+				<ListItem Item ={Item} userId={user_id}/>
+			{/each}
+		</div>
+		<div class="sidePanel">
+			<h2 class="sidePanelHeading">Filters</h2>
+			<!-- <div class="flex flex-col"> -->
+				<div class="flex flex-row m-4">
+					<select class="select select-bordered w-full max-w-xs" on:change={filterFeed}>
+						<option disabled selected>Publishers</option>
+							{#each publishers.Items as publisher}
+								<option>{publisher.publisher}</option>
+							{/each}
+					</select>
+				</div>
+			<!-- </div> -->
+		</div>
 	{/if}
 </div>
+
+<style>
+	.sidePanel {
+		margin-left: 2;
+		width: 30%;
+		height: 100%;
+		overflow-x: hidden;
+		padding-top: 20px;
+		display: flexbox;
+	}
+
+	.sidePanelHeading {
+		padding: 16px;
+		color: #1a202c;
+    	font-size: 1.25rem;
+    	line-height: 1.25;
+		font-weight: 700;
+	}
+
+	.notFoundText {
+		text-align: center;
+		margin-top: 20%;
+		font-size: 1.25rem;
+		line-height: 1.25;
+		font-weight: 700;
+	}
+</style>
