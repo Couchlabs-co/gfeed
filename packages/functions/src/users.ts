4e8391f9e2b693717406684614575eba7d3eee9b
Jasdeep Madan <jmadan@protonmail.com>
2023-10-02 16:06:12 +0530

    fixed user creation for google vs auth0 login

--- packages/functions/src/users.ts
+++ packages/functions/src/users.ts
@@ -5,51 +5,65 @@ import { Table } from "sst/node/table";
 // import * as uuid from "uuid";
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 
-export const handler = ApiHandler(async (evt: APIGatewayProxyEventV2) => {
-  console.log("evt time: ", evt.requestContext.time);
-  const {user} = JSON.parse(evt.body ?? '');
-
-  if(!user || !user.authId) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({"message": "Bad Request"})
-    };
-  }
-
-  try {
-    const [channel, authId] = user.authId.split('|');
-    const userTable = Table.user.tableName;
+const createUser = async (id: string, name: string, email: string, channel: string) => {
+  const userTable = Table.user.tableName;
     const userRecord: Record<string, AttributeValue> = {
-      id: { S: authId },
-      name: { S: user.name },
-      email: { S: user.email },
+      id: { S: id },
+      name: { S: name },
+      email: { S: email },
       channel: { S: channel },
     };
     const command = new UpdateItemCommand({
       TableName: userTable,
       Key: {
-        email: { S: user.email },
+        email: { S: email },
       },
-      UpdateExpression: "set #id = :id, #name = :name, #email = :email, #channel = :channel",
+      UpdateExpression: "set #id = :id, #name = :name, #channel = :channel",
       ExpressionAttributeNames: {
         "#id": "id",
         "#name": "name",
-        "#email": "email",
         "#channel": "channel",
       },
       ExpressionAttributeValues: {
-        ":name": user.name, 
-        ":email": user.email,
-        ":channel": channel,
+        ":id": userRecord.id,
+        ":name": userRecord.name, 
+        ":channel": userRecord.channel,
       },
       ReturnValues: "ALL_NEW",
     });
 
     const res = await dbClient.send(command);
-    console.log("res........", res);
+    return res;
+}
+
+export const handler = ApiHandler(async (evt: APIGatewayProxyEventV2) => {
+  console.log("evt time: ", evt.requestContext.time);
+
+  console.log("evt.body: ", evt.body);
+  const {user} = JSON.parse(evt.body ?? '');
+
+  if(!user || !user.email) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({"message users": "Bad Request"})
+    };
+  }
+
+  try {
+    if(user.id){
+      const res = await createUser(user.id, user.name, user.email, "google");
+      return {
+        statusCode: 201,
+        body: JSON.stringify({"message": "Success", "user": res})
+      }
+    }
+
+    const [channel, authId] = user.authId.split('|');
+    const res = await createUser(authId, user.name, user.email, channel);
+
     return {
       statusCode: 201,
-      body: JSON.stringify({"msg": "Success", "user": res})
+      body: JSON.stringify({"message": "Success", "user": res})
     };
     
   } catch (err) {
