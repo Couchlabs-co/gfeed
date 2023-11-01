5738656e1cc48288206954e44fbdff1bb6c35473
Jasdeep Madan <jmadan@protonmail.com>
2023-11-01 23:43:58 +1100

    margin for footer

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<div class="container mx-auto">
+<div class="container m-2">
 	<footer class="footer footer-center p-4 bg-white text-base-content">
 		<div>
 		  <p>Copyright © 2023 - All right reserved by IntelliFeed</p>
