ae18ee6e8e428d2d1663da192ebc3a90bfc99c32
Jasdeep Madan <jmadan@protonmail.com>
2023-11-26 22:42:58 +1100

    ignoring show HN articles from hacker news and refactored feedhandler to have Save item function

--- packages/functions/src/feedHandler.ts
+++ packages/functions/src/feedHandler.ts
@@ -20,42 +20,10 @@ export async function main(event: SQSEvent) {
     for (const item of rssItems) {
       try {
         const feedItem = await formatItem(item, publisher, tag);
-        const putParams = new UpdateItemCommand({
-          TableName: tableName,
-          Key: {
-            publishedDate: feedItem.publishedDate,
-            guid: feedItem.guid,
-          },
-          UpdateExpression: "set #id = :id, #title = :title, #link = :link, #author = :author, #keywords = :keywords, #pubDate = :pubDate, #content = :content, #publisher = :publisher, #publisherId = :publisherId, #img = :img, #tag = :tag",
-          ExpressionAttributeNames: {
-            "#id": "id",
-            "#title": "title",
-            "#link": "link",
-            "#author": "author",
-            "#keywords": "keywords",
-            "#pubDate": "pubDate",
-            "#content": "content",
-            "#publisher": "publisher",
-            "#publisherId": "publisherId",
-            "#tag": "tag",
-            "#img": "img",
-          },
-          ExpressionAttributeValues: {
-            ":id": { S: v4() },
-            ":title": feedItem.title,
-            ":link": feedItem.link,
-            ":author": feedItem.author,
-            ":keywords": feedItem.keywords,
-            ":pubDate": feedItem.pubDate,
-            ":content": feedItem.content,
-            ":publisher": feedItem.publisher,
-            ":publisherId": publisherId,
-            ":tag": feedItem.tag,
-            ":img": feedItem.img ?? '',
-          },
-          ReturnValues: "ALL_NEW",
-        });
-        await dbClient.send(putParams);
+        if(publisher === 'Hacker News' && feedItem.title.S?.includes('Show HN')) {
+          continue;
+        }
+        await SaveItem(tableName, feedItem, publisherId);
       } catch (err) {
         console.log("err........", publisher, feedUrl, err, item.title);
       }
@@ -68,3 +36,42 @@ export async function main(event: SQSEvent) {
     body: JSON.stringify({ status: "successful" }),
   };
 }
+async function SaveItem(tableName: string, feedItem: any, publisherId: any) {
+  const putParams = new UpdateItemCommand({
+    TableName: tableName,
+    Key: {
+      publishedDate: feedItem.publishedDate,
+      guid: feedItem.guid,
+    },
+    UpdateExpression: "set #id = :id, #title = :title, #link = :link, #author = :author, #keywords = :keywords, #pubDate = :pubDate, #content = :content, #publisher = :publisher, #publisherId = :publisherId, #img = :img, #tag = :tag",
+    ExpressionAttributeNames: {
+      "#id": "id",
+      "#title": "title",
+      "#link": "link",
+      "#author": "author",
+      "#keywords": "keywords",
+      "#pubDate": "pubDate",
+      "#content": "content",
+      "#publisher": "publisher",
+      "#publisherId": "publisherId",
+      "#tag": "tag",
+      "#img": "img",
+    },
+    ExpressionAttributeValues: {
+      ":id": { S: v4() },
+      ":title": feedItem.title,
+      ":link": feedItem.link,
+      ":author": feedItem.author,
+      ":keywords": feedItem.keywords,
+      ":pubDate": feedItem.pubDate,
+      ":content": feedItem.content,
+      ":publisher": feedItem.publisher,
+      ":publisherId": publisherId,
+      ":tag": feedItem.tag,
+      ":img": feedItem.img ?? '',
+    },
+    ReturnValues: "ALL_NEW",
+  });
+  await dbClient.send(putParams);
+}
+
