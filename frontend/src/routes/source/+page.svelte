9f18122f334c423d5f5b9e29e048259328a726df
Jasdeep Madan <jmadan@protonmail.com>
2023-12-23 23:26:34 +1100

    wip Needs work on UI

--- frontend/src/routes/source/+page.svelte
+++ frontend/src/routes/source/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-    import { readingListStore } from '../../store';
+    import { gFeedStore } from '../../store';
     import { Star, StarOff, ShieldCheck } from 'lucide-svelte';
-    import { userAction } from '$lib/userActions';
+    import { userAction } from '../../lib/userActions';
 
 	export let data;
     
@@ -10,7 +10,7 @@
     let dislikeIndex = -1;
 
     const { Items } = data.data;
-        $readingListStore.sources = Items.lenght? Items: [];
+        $gFeedStore.sources = Items.lenght? Items: [];
     let pageIndex = 0;
     let pageSize = 10;
     const totalPages = Math.ceil(Items.length / pageSize);
@@ -39,12 +39,13 @@
     }
 
 </script>
-<div>
-    <h3 class="mt-2 mb-2">
-        Some of the sources we pull data from to make sure you have latest content
-    </h3>
-    <div class="overflow-x-auto h-max w-9/12 items-center place-content-center m-auto">
-        <table class="table">
+<div class="flex flex-col justify-center items-center">
+    <div class="text-3xl mt-4">
+		<p class="text-2xl">Start following your favourite websites</p>
+        <p class="text-sm">Content that matters to you and enjoy the best in a single place.</p>
+	</div>
+    <div class="h-max w-9/12 mt-4 border border-black">
+        <table class="table-md border border-black">
         <thead>
           <tr>
             <th class="text-2xl">Publishers</th>
@@ -55,17 +56,18 @@
                 <tr>
                     <td class="w-60">
                         {#if item.logo}
-                            <img src={item.logo} alt={item.name} width="32" height="32"/>
+                            <img src={item.logo} alt={item.name} width="62" height="20"/>
                         {:else}
-                            <div class="avatar placeholder">
+                            <!-- <div class="avatar placeholder">
                                 <div class="bg-neutral-focus text-neutral-content rounded w-10">
                                     <span>{item.name}</span>
                                 </div>
-                            </div> 
+                            </div>  -->
+                            <span class="text-xl font-semibold">{item.name}</span>
                         {/if}
                     </td>
 
-                    <td class="w-96"><span class="text-xl font-semibold">{item.name}</span></td>
+                    <!-- <td class="w-96"><span class="text-xl font-semibold">{item.name}</span></td> -->
                     <td class="w-20">
                         <button class={i === likeIndex ? "btn btn-square btn-outline bg-success": "btn btn-square btn-outline"}
                             on:click={(e) => handleClick(e, item.name, "likes", "publisher", i, item.publisherId, item.feedUrl)}>
@@ -77,11 +79,9 @@
                             on:click={(e) => handleClick(e, item.name, "dislikes", "publisher", i, item.publisherId, item.feedUrl)}>
                             <StarOff color={i === dislikeIndex ? "white": "black"}/>
                         </button>
-                        {#if session?.user?.email === "jasdeepm@gmail.com"}
                             <button class="btn btn-square btn-outline">
                                 <ShieldCheck />
                             </button>
-                        {/if}
                     </td>
                 </tr>
             {/each}
