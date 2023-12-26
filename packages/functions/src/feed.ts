be894f75dd0a6c45f3099a6f6a5a8bad6803054d
Jasdeep Madan <jmadan@protonmail.com>
2023-12-27 00:28:23 +1100

    refactored feed function to consider user algo preference

--- packages/functions/src/feed.ts
+++ packages/functions/src/feed.ts
@@ -6,14 +6,15 @@ import {jwtDecode} from "jwt-decode";
 import * as jose from 'jose';
 const he = require('he');
 
-async function GetFeed(dateKeys: string[]) {
+async function GetFeedTimeBased() {
+  const keyDates = getFeedDates();
   const result = {
-    Count: 0,
+    Count: 0,    
     Items: <any>[],
   };
 
   await Promise.all(
-    dateKeys.map(async (key) => {
+    keyDates.map(async (key) => {
       const command: QueryCommand = new QueryCommand({
         TableName: Table.posts.tableName,
         KeyConditionExpression: "publishedDate = :publishedDate",
@@ -31,8 +32,59 @@ async function GetFeed(dateKeys: string[]) {
   return result;
 }
 
-async function GetUserFeed(dateKeys: string[], userId: string) {
+async function GetFeedInterestBased(userInterests: any) {
+  
   const result = {
+    Count: 0,    
+    Items: <any>[],
+  };
+
+  if(userInterests && userInterests.length > 0){
+    await Promise.all(
+      userInterests.map(async (interest: any) => {
+        const command: QueryCommand = new QueryCommand({
+          TableName: Table.posts.tableName,
+          IndexName: 'tagIndex',
+          KeyConditionExpression: "tag = :tag",
+          ExpressionAttributeValues: {
+            ":tag": { S: interest.content.S ?? 'Misc' },
+          },
+        });
+        let { Count, Items } = await dbClient.send(command);
+        result.Count += Count ?? 0;
+        result.Items = result.Items.concat(Items);
+      })
+    );
+  }
+
+  // const publishersUserLikes = userInterests.Items?.filter((item: any) => {
+  //   return item.userAction.S === "likes" && item.contentType.S === "publisher";
+  // });
+  const publishersUserDislikes = userInterests.Items?.filter((item: any) => {
+    return item.userAction.S === "dislikes" && item.contentType.S === "publisher";
+  });
+
+  if(publishersUserDislikes && publishersUserDislikes?.length !== 0) {
+    for(const publisher of publishersUserDislikes) {
+      result.Items = result.Items.filter((item: any) => {
+        return item.publisher?.S !== publisher.content.S;
+      });
+    }
+  }
+  
+  // if(interestsUserFollows && interestsUserFollows?.length !== 0) {
+  //   for(const interest of interestsUserFollows) {
+  //     result.Items.filter((item: any) => {
+  //       return item.tag?.S.toLowerCase() === interest.content.S?.toLowerCase() || item.tag?.S.toLowerCase() === 'misc';
+  //     });
+  //   }
+  // }
+
+  return result;
+}
+
+async function GetUserFeed(dateKeys: string[], userId: string) {
+  let result = {
     Count: 0,
     Items: <any>[],
   };
@@ -49,72 +101,22 @@ async function GetUserFeed(dateKeys: string[], userId: string) {
     return item.userAction.S === "follow" && item.contentType.S === "interest";
   });
 
-  if(interestsUserFollows && interestsUserFollows.length > 0){
-    await Promise.all(
-      interestsUserFollows.map(async (interest) => {
-        const command: QueryCommand = new QueryCommand({
-          TableName: Table.posts.tableName,
-          IndexName: 'tagIndex',
-          KeyConditionExpression: "tag = :tag",
-          ExpressionAttributeValues: {
-            ":tag": { S: interest.content.S ?? 'Misc' },
-          },
-        });
-        let { Count, Items } = await dbClient.send(command);
-        result.Count += Count ?? 0;
-        result.Items = result.Items.concat(Items);
-      })
-    );
-  }
-
-
-  // const userInterestFeedCommand: QueryCommand = new QueryCommand({
-  //   TableName: Table.posts.tableName,
-  //   IndexName: 'tagIndex',
-  //   KeyConditionExpression: "tag = :tag",
-  //   ExpressionAttributeValues: {
-  //     ":tag": { S: key },
-  //   },
-  //   ConsistentRead: true,
-  // });
-  
-  // await Promise.all(
-  //   dateKeys.map(async (key) => {
-  //     const command: QueryCommand = new QueryCommand({
-  //       TableName: Table.posts.tableName,
-  //       KeyConditionExpression: "publishedDate = :publishedDate",
-  //       ExpressionAttributeValues: {
-  //         ":publishedDate": { S: key },
-  //       },
-  //       ConsistentRead: true,
-  //     });
-  //     let { Count, Items } = await dbClient.send(command);
-  //     result.Count += Count ?? 0;
-  //     result.Items = result.Items.concat(Items);
-  //   })
-  // );
-
-
-  const publishersUserLikes = userInterests.Items?.filter((item: any) => {
-    return item.userAction.S === "likes" && item.contentType.S === "publisher";
-  });
-  const publishersUserDislikes = userInterests.Items?.filter((item: any) => {
-    return item.userAction.S === "dislikes" && item.contentType.S === "publisher";
+  const userAlgoPreference = userInterests.Items?.filter((item: any) => {
+    return item.userAction.S === "selected" && item.content.S === "feedAlgorithm";
   });
 
-  if(publishersUserDislikes && publishersUserDislikes?.length !== 0) {
-    for(const publisher of publishersUserDislikes) {
-      result.Items = result.Items.filter((item: any) => {
-        return item.publisher?.S !== publisher.content.S;
-      });
-    }
-  }
-  
-  if(interestsUserFollows && interestsUserFollows?.length !== 0) {
-    for(const interest of interestsUserFollows) {
-      result.Items.filter((item: any) => {
-        return item.tag?.S.toLowerCase() === interest.content.S?.toLowerCase() || item.tag?.S.toLowerCase() === 'misc';
-      });
+  // console.log(userAlgoPreference[0].contentType.S);
+
+  if(userAlgoPreference && userAlgoPreference.length > 0){
+    switch(userAlgoPreference[0].contentType.S){
+      case 'timeBased': {
+        result = await GetFeedTimeBased();
+        break
+      }
+      case 'interestBased': {
+        result = await GetFeedInterestBased(interestsUserFollows);
+        break;
+      }
     }
   }
 
@@ -133,22 +135,8 @@ export const handler = ApiHandler(async (evt) => {
   if(evt.headers.authorization) {
     const token = evt.headers.authorization?.split(" ")[1];
     try {
-      const tokenHeader = await jwtDecode(token, { header: true });
-      const JWKS = jose.createRemoteJWKSet(new URL('https://readingcorner.au.auth0.com/.well-known/jwks.json'));
-
-      const { payload, protectedHeader } = await jose.jwtVerify(token, JWKS, {
-        issuer: 'https://readingcorner.au.auth0.com/',
-        audience: 'https://api.readingcorner.com',
-      })
-      if(tokenHeader.kid !== protectedHeader.kid) {
-        return {
-          statusCode: 401,
-          body: JSON.stringify({ error: "Unauthorized" }),
-        }
-      }
-      const sub = payload.sub ?? "";
-      const userId = sub.split("|").length > 1 ? sub?.split("|")[1] : sub;
-
+      const userId = await getUserFromToken(token);
+      console.log('userId', userId);
       result = await GetUserFeed(keyDates, userId);
     }
     catch(err) {
@@ -161,7 +149,7 @@ export const handler = ApiHandler(async (evt) => {
       }
     }
   } else {
-    result = await GetFeed(keyDates);
+    result = await GetFeedTimeBased();
   }
   const feedItems = <any>[];
 
@@ -198,5 +186,21 @@ function getFeedDates() {
     keys.push(new Date(dt.setDate(dt.getDate() - i)).toISOString().split("T")[0]);
   }
   return keys;
+}
+
+async function getUserFromToken(token: string) {
+  const tokenHeader = await jwtDecode(token, { header: true });
+      const JWKS = jose.createRemoteJWKSet(new URL('https://readingcorner.au.auth0.com/.well-known/jwks.json'));
+
+      const { payload, protectedHeader } = await jose.jwtVerify(token, JWKS, {
+        issuer: 'https://readingcorner.au.auth0.com/',
+        audience: process.env.AUTH0_API_AUDIENCE ?? '',
+      })
+      if(tokenHeader.kid !== protectedHeader.kid) {
+        throw new Error('Unforbidden');
+      }
+      const sub = payload.sub ?? "";
+      const userId = sub.split("|").length > 1 ? sub?.split("|")[1] : sub;
+      return userId;
 }
 
--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -1,9 +1,13 @@
-import { StackContext, use, Api } from "sst/constructs";
+import { StackContext, use, Api, Config } from "sst/constructs";
 import { DbStack } from "./DbStack";
 import { SsrDomainProps } from "sst/constructs/SsrSite";
 
 export function ApiStack({ stack, app }: StackContext) {
   let customDomain: SsrDomainProps | undefined;  
+
+  const Auth0APIAudience = new Config.Parameter(stack, "AUTH0_API_AUDIENCE", {
+    value: process.env.AUTH0_API_AUDIENCE ?? '',
+  });
 
   switch(stack.stage) {
     case "prod": {
@@ -43,7 +47,14 @@ export function ApiStack({ stack, app }: StackContext) {
     accessLog:
       "$context.identity.sourceIp,$context.requestTime,$context.httpMethod,$context.routeKey,$context.protocol,$context.status,$context.responseLength,$context.requestId",
     routes: {
-      "POST /feed": "packages/functions/src/feed.handler",
+      "POST /feed": {
+        function : {
+          handler: "packages/functions/src/feed.handler",
+          environment: {
+            AUTH0_API_AUDIENCE: Auth0APIAudience.value
+          }
+        }
+      },
       "POST /users": "packages/functions/src/createUser.handler",
       "GET /users/{userId}": "packages/functions/src/getUser.handler",
       "GET /users/{userId}/interests": "packages/functions/src/userInterests.handler",
