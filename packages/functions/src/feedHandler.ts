5df52ce87adfa4e6c00c2728238904d53d7611f2
Jasdeep Madan <jmadan@protonmail.com>
2024-01-03 22:35:37 +1100

    trying to fix missing processing of batch messages from sqs and also adding deadletter queue

--- packages/functions/src/feedHandler.ts
+++ packages/functions/src/feedHandler.ts
@@ -1,16 +1,24 @@
 import { SQSEvent, SQSRecord } from "aws-lambda";
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
 import { UpdateItemCommand } from "@aws-sdk/client-dynamodb";
 import { dbClient } from "./utils/dbClient";
 import { Table } from "sst/node/table";
 import fetchRSSFeed from "./utils/fetchRssFeed";
 import { formatItem } from "./utils/formatItem";
 import { v4 } from "uuid";
+import { Queue } from "sst/node/queue";
+import { DateTime } from "luxon";
+
+const sqs = new SQSClient({
+  region: "ap-southeast-2",
+});
 
 export async function main(event: SQSEvent) {
-  const startTime = Date.now();
   const tableName = Table.posts.tableName;
   const records: SQSRecord[] = event.Records;
+  console.log('length of records from queue: ', records.length);
   for(const record of records){
+    const startTime = Date.now();
     const { id: publisherId, publisher, feedUrl, tag, feedType } = JSON.parse(record.body);
     if(feedType === 'html') {
       console.log(`skipping html feed: ${publisher} with url: ${feedUrl}`);
@@ -32,16 +40,13 @@ export async function main(event: SQSEvent) {
         const feedItem = await formatItem(item, publisher, tag);
         await SaveItem(tableName, feedItem, publisherId);
       } catch (err) {
-        console.log("feedHandler err........", publisher, feedUrl, err, JSON.stringify(item));
+        console.log("feedHandler err........", publisher, feedUrl, err, item.title);
+        // await ItemToDeadLetterQ(item);
       }
     }
-    console.log(`Message processed: "${publisher}"`);
+    console.log(`Message processed: ${publisher} with url: ${feedUrl}`);
     const endTime = Date.now();
     console.log(`total time taken: ${endTime - startTime}`)
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ status: "successful" }),
-    };
   }
     
   return {
@@ -50,15 +55,19 @@ export async function main(event: SQSEvent) {
   };
 }
 async function SaveItem(tableName: string, feedItem: any, publisherId: any) {
+  const pkDate = DateTime.fromISO(feedItem.publishedDate.S);
+  const pk = Number(`${pkDate.year}${pkDate.toFormat("MM")}`);
+
   const putParams = new UpdateItemCommand({
     TableName: tableName,
     Key: {
-      publishedDate: feedItem.publishedDate,
+      pk: { N: `${pk}` },
       guid: feedItem.guid,
     },
-    UpdateExpression: "set #id = :id, #title = :title, #link = :link, #author = :author, #keywords = :keywords, #pubDate = :pubDate, #content = :content, #publisher = :publisher, #publisherId = :publisherId, #img = :img, #tag = :tag",
+    UpdateExpression: "set #id = :id, #publishedDate = :publishedDate, #title = :title, #link = :link, #author = :author, #keywords = :keywords, #pubDate = :pubDate, #content = :content, #publisher = :publisher, #publisherId = :publisherId, #img = :img, #tag = :tag",
     ExpressionAttributeNames: {
       "#id": "id",
+      "#publishedDate": "publishedDate",
       "#title": "title",
       "#link": "link",
       "#author": "author",
@@ -72,6 +81,7 @@ async function SaveItem(tableName: string, feedItem: any, publisherId: any) {
     },
     ExpressionAttributeValues: {
       ":id": { S: v4() },
+      ":publishedDate": feedItem.publishedDate,
       ":title": feedItem.title,
       ":link": feedItem.link,
       ":author": feedItem.author,
@@ -86,5 +96,14 @@ async function SaveItem(tableName: string, feedItem: any, publisherId: any) {
     ReturnValues: "ALL_NEW",
   });
   await dbClient.send(putParams);
+}
+
+async function ItemToDeadLetterQ(item: any) {
+  const params = {
+    MessageBody: JSON.stringify(item),
+    QueueUrl: Queue.PostDLQ.queueUrl,
+  };
+  await sqs.send(new SendMessageCommand(params));
+  console.log("Sent message to queue", params);
 }
 
