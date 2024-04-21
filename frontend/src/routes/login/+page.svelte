a76605f498182ddd4fbbad7efafa021d6e75407e
Jasdeep Madan <jmadan@protonmail.com>
2024-04-21 23:18:15 +1000

    fixing deployment issue (#18)

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,18 +1,10 @@
 <script lang="ts">
-	import { signIn } from "@auth/sveltekit/client";
     import { page } from "$app/stores";
 
 </script>
 
 <div class="max-h-full content-center my-40 mx-auto">
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={() => signIn(
-        'auth0', {
-            redirect: false,
-            callbackUrl: `${$page.url.origin}`,
-        },
-        {
-            scope: 'api openid profile email offline_access'
-        })}>
+    <a href="/api/auth/login?post_login_url={$page.url.origin}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="button"> 
         Sign In
-    </button>
+    </a>
 </div>
