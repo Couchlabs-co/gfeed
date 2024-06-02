a27a43a09a04e9525091f7cebc07bbab6a415b10
Jasdeep Madan <jmadan@protonmail.com>
2024-06-02 22:49:40 +1000

    removed org name values from configs (#19)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
 	plugins: [
 		sentrySvelteKit({
 				sourceMapsUploadOptions: {
-				org: "couchlabs",
 				project: "gfeed",
 				authToken: process.env.SENTRY_AUTH_TOKEN,
       },
